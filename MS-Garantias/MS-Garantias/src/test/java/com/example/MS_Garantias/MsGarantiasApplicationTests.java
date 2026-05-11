fe59f580f62125213ada6f66a3e1057167ce1682
Cristofer Figueroa <cristo.figueroam@duocuc.cl>
2026-05-11 19:43:36 -0400

    Add files via upload

---
+++ MS-Garantias/MS-Garantias/src/test/java/com/example/MS_Garantias/MsGarantiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MS_Garantias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGarantiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
